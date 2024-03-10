b5a677de988b2e33ef91b83cd9d5124ad3a4a742
Sergey Popov <spopov@shakuro.com>
2024-03-10 22:46:16 +0300

    II-15-[added native progress animarion]

--- PullToRefreshSample/PullToRefreshSampleiOS/UI/ScrollContentView.swift
+++ PullToRefreshSample/PullToRefreshSampleiOS/UI/ScrollContentView.swift
@@ -18,12 +18,27 @@ struct ScrollContentView: View {
                 })
             },
             pullingViewBuilder: { (progress) in
-                LottieView(animation: .named("animation-pulling-shakuro_logo"))
-                    .playbackMode(.paused(at: .progress(progress)))
+                Circle()
+                    .trim(from: 0, to: progress * 0.9)
+                    .stroke(Color.green, lineWidth: 5)
+                    .frame(width: 100, height: 100)
+                    .rotationEffect(Angle(degrees: 360))
+
+                // TODO: implement
+//                LottieView(animation: .named("animation-pulling-shakuro_logo"))
+//                    .playbackMode(.paused(at: .progress(progress)))
             },
             refreshingViewBuilder: { (isTriggered) in
-                LottieView(animation: .named("animation-refreshing-shakuro_logo"))
-                    .playbackMode(isTriggered ? .playing(.fromProgress(0, toProgress: 1, loopMode: .loop)) : .paused)
+                Circle()
+                    .trim(from: 0, to: 0.9)
+                    .stroke(Color.green, lineWidth: 5)
+                    .frame(width: 100, height: 100)
+                    .rotationEffect(Angle(degrees: isTriggered ? 360 : 0))
+                    .animation(isTriggered ? .linear(duration: 1).repeatForever(autoreverses: false) : nil, value: isTriggered)
+
+                // TODO: implement
+//                LottieView(animation: .named("animation-refreshing-shakuro_logo"))
+//                    .playbackMode(isTriggered ? .playing(.fromProgress(0, toProgress: 1, loopMode: .loop)) : .paused)
             },
             contentViewBuilder: { _ in
                 Rectangle()
