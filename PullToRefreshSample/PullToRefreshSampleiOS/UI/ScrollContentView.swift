e062308c5515648b61513a9c08c2112d9c2be82f
Sergey Popov <spopov@shakuro.com>
2024-03-11 08:28:22 +0300

    II-15-[reverted animation]

--- PullToRefreshSample/PullToRefreshSampleiOS/UI/ScrollContentView.swift
+++ PullToRefreshSample/PullToRefreshSampleiOS/UI/ScrollContentView.swift
@@ -6,6 +6,8 @@ struct ScrollContentView: View {
 
     @State private var isRefreshing: Bool = false
 
+//    @State var rotationDegrees: CGFloat = 0.0
+
     var body: some View {
         let options = PullToRefreshScrollViewOptions()
         PullToRefreshScrollView(
@@ -18,27 +20,31 @@ struct ScrollContentView: View {
                 })
             },
             pullingViewBuilder: { (progress) in
-                Circle()
-                    .trim(from: 0, to: progress * 0.9)
-                    .stroke(Color.green, lineWidth: 5)
-                    .frame(width: 100, height: 100)
-                    .rotationEffect(Angle(degrees: 360))
+//                Circle()
+//                    .trim(from: 0, to: progress * 0.9)
+//                    .stroke(Color.green, lineWidth: 5)
+//                    .frame(width: 100, height: 100)
+//                    .rotationEffect(Angle(degrees: 270))
 
                 // TODO: implement
-//                LottieView(animation: .named("animation-pulling-shakuro_logo"))
-//                    .playbackMode(.paused(at: .progress(progress)))
+                LottieView(animation: .named("animation-pulling-shakuro_logo"))
+                    .playbackMode(.paused(at: .progress(progress)))
             },
             refreshingViewBuilder: { (isTriggered) in
-                Circle()
-                    .trim(from: 0, to: 0.9)
-                    .stroke(Color.green, lineWidth: 5)
-                    .frame(width: 100, height: 100)
-                    .rotationEffect(Angle(degrees: isTriggered ? 360 : 0))
-                    .animation(isTriggered ? .linear(duration: 1).repeatForever(autoreverses: false) : nil, value: isTriggered)
+//                Circle()
+//                    .trim(from: 0, to: 0.9)
+//                    .stroke(Color.green, lineWidth: 5)
+//                    .frame(width: 100, height: 100)
+//                    .rotationEffect(Angle(degrees: 270))
+//                    .rotationEffect(Angle(degrees: rotationDegrees))
+//                    .animation(.linear(duration: 5).repeatForever(autoreverses: false), value: rotationDegrees)
+//                    .onAppear(perform: {
+//                        rotationDegrees = 360
+//                    })
 
                 // TODO: implement
-//                LottieView(animation: .named("animation-refreshing-shakuro_logo"))
-//                    .playbackMode(isTriggered ? .playing(.fromProgress(0, toProgress: 1, loopMode: .loop)) : .paused)
+                LottieView(animation: .named("animation-refreshing-shakuro_logo"))
+                    .playbackMode(isTriggered ? .playing(.fromProgress(0, toProgress: 1, loopMode: .loop)) : .paused)
             },
             contentViewBuilder: { _ in
                 Rectangle()
