072fb5d6059cffaec3866a83d020aa85f650d66b
ntryshkina <ntryshkina@shakuro.com>
2024-04-10 15:26:57 +0300

    II-29-[CircularProgressView]

--- PullToRefreshSample/PullToRefreshSampleiOS/UI/CircularProgressView.swift
+++ PullToRefreshSample/PullToRefreshSampleiOS/UI/CircularProgressView.swift
@@ -2,22 +2,35 @@ import SwiftUI
 
 struct CircularProgressView: View {
 
-    private var progress: CGFloat = 0.0
+    private let tintColor: Color
+    private let dashesesCount: Int
+    private let size: CGFloat
+    private let totalDashesCount = 8
 
-    init(progress: CGFloat) {
-        self.progress = progress
+    init(progress: CGFloat, tintColor: Color = .secondary, size: CGFloat = 20.0) {
+        self.dashesesCount = Int(CGFloat(totalDashesCount) * progress)
+        self.tintColor = tintColor
+        self.size = size
     }
 
     var body: some View {
-        StoppedActivityIndicator()
-            .rotationEffect(Angle(degrees: 90))
-            .rotationEffect(Angle(degrees: progress * 360.0))
+        ZStack {
+            ForEach(0..<dashesesCount, id: \.self, content: { dashNumber in
+                Capsule()
+                    .fill(tintColor)
+                    .frame(width: size / CGFloat(totalDashesCount), height: size / 3.0)
+                    .transformEffect(CGAffineTransform(translationX: 0, y: size / -3.0))
+                    .rotationEffect(Angle(radians: CGFloat.pi * 2 * CGFloat(dashNumber) / CGFloat(totalDashesCount)))
+            })
+        }
+        .frame(width: size, height: size)
+        .opacity(0.7)
     }
 
 }
 
 #Preview {
-    CircularProgressView(progress: 0)
+    CircularProgressView(progress: 0.8)
 }
 
 private struct StoppedActivityIndicator: UIViewRepresentable {
